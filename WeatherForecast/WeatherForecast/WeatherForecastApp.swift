80958d6601e309daa048cdf8054257b3202f4420
Denis Kutlubaev <denis.kutlubaev@hungerstation.com>
2023-02-26 13:24:22 +0400

    feat: create project, add Model, Model.json based on open-meteo API

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Denis Kutlubaev on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
