df33332cdc17050bd11de2479574f8b06d8dd68a
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2024-10-03 21:13:57 +0400

    ci: add an error prone code to test Codex

---
+++ WeatherForecast/WeatherForecast/Utils/Codex.swift
@@ -0,0 +1,12 @@
+//
+//  Codex.swift
+//  WeatherForecast
+//
+//  Created by Denis Kutlubaev on 03/10/2024.
+//
+
+import Foundation
+
+func divide(a: Double, b: Double) -> Double {
+    return a / b
+}
